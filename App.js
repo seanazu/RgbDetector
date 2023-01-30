be6b94b16ad79f53373a6dbccc437bc04c4c892e
Sean Azulay <seanazu8@gmail.com>
2023-01-31 00:23:54 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        
+    }
+})
